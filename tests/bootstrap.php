1eadbbb3a3983472d8cdfcfc005b2eae1ea52cca
Ivan Novakov <ivan.novakov@fel.cvut.cz>
2013-05-14 09:42:10 +0200

    path fix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,7 @@
 
 use ZfcShib\Module;
 
-require __DIR__ . '/../../../vendor/autoload.php';
+require __DIR__ . '/../../../autoload.php';
 require __DIR__ . '/../Module.php';
 
 $module = new Module();
